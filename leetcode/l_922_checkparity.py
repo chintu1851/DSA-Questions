bb6815bb37002ef758b53a8b48161c6880bbe371
chintu1851 <patelchintan1851@gmail.com>
2025-03-25 16:54:48 -0400

    add code

--- leetcode/l_922_checkparity.py
+++ leetcode/l_922_checkparity.py
@@ -1,5 +1,17 @@
 def checkparity(nums):
-    
-    print(nums)
+    i=0
+    j=1
+    n=len(nums)-1
+    while i<n and j<n:
+        if nums[i] % 2 == 0:  # If even number is at even index, move forward
+            i += 2
+        elif nums[j] % 2 == 1:  # If odd number is at odd index, move forward
+            j += 2
+        else:
+            # Swap the numbers at incorrect positions
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 2
+            j += 2  
+    print(nums)    
 nums = [4,2,5,7]
 checkparity(nums)
