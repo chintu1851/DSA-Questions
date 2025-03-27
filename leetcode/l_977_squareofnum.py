93ff8afb4d053f3980c324aa973bfe355d5e7b93
chintu1851 <patelchintan1851@gmail.com>
2025-03-26 23:17:24 -0400

    add code

--- leetcode/l_977_squareofnum.py
+++ leetcode/l_977_squareofnum.py
@@ -1,5 +1,18 @@
 def square(nums):
-    print([num*num for num in nums])
-    
-nums = [-4,-1,0,3,10]
+    left=0
+    right=len(nums)-1
+    result=[0]*len(nums)
+    index = len(nums) - 1
+    while left<=right:
+        leftside=nums[left]*nums[left]
+        rightside=nums[right]*nums[right]
+        if leftside<rightside:
+            result[index] = rightside
+            right-=1
+        else:
+            result[index]=leftside
+            left+=1
+        index-=1
+    print(result)
+nums = [-7,-3,2,3,11]
 square(nums)
