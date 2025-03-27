7675c6fe3366d9732ea919ee990649ca05e6a140
chintu1851 <patelchintan1851@gmail.com>
2025-03-26 22:12:14 -0400

    add code

--- leetcode/l_942_dimatchingpermutation.py
+++ leetcode/l_942_dimatchingpermutation.py
@@ -1,9 +1,16 @@
 def matchdi(s):
     low=0
-    high=len(s)-1
+    high=len(s)
     result=[]
     for i in s:
         if i=="I":
-            
+            result.append(low)
+            low+=1
+        else:
+            result.append(high)
+            high-=1
+    result.append(low) 
+    print(result)
+    
 s = "IDID"
 matchdi(s)
