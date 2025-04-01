75d877eb6ba59414f3f097cc9c572011d36f3e56
chintu1851 <patelchintan1851@gmail.com>
2025-04-01 00:30:34 -0400

    add code

--- leetcode/l_2108_palindromic.py
+++ leetcode/l_2108_palindromic.py
@@ -1,3 +1,9 @@
 def palindromic(words):
     print(words)
+    for i in words:
+        s=i[::-1]
+        if i==s:
+            return i
+    return ""
 words = ["abc","car","ada","racecar","cool"]
+print(palindromic(words))
