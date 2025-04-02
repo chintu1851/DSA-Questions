0e500043b514c5c890384264b14f38afc2b309dd
chintu1851 <patelchintan1851@gmail.com>
2025-04-02 11:45:13 -0400

    add code

--- leetcode/l_2367_triplets.py
+++ leetcode/l_2367_triplets.py
@@ -1,6 +1,12 @@
 def triplets(nums,diff):
-    print(nums)
-
+        seen = {}
+        counter = 0
+        for i in range(len(nums)):
+            seen[nums[i]] = i
+        for num in nums:
+            if (num + diff) in seen and (num + 2 * diff) in seen:
+                counter += 1
+        return counter
 nums = [0,1,4,6,7,10]
 diff = 3
-triplets(nums,diff)
+print(triplets(nums,diff))
