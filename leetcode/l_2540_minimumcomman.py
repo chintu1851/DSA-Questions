1c0ad868d2a952ec7e6496b25356ff9c0cb4fcb8
chintu1851 <patelchintan1851@gmail.com>
2025-04-03 23:21:47 -0400

    add code

--- leetcode/l_2540_minimumcomman.py
+++ leetcode/l_2540_minimumcomman.py
@@ -1,4 +1,14 @@
 def minimumcommn(nums1,nums2):
-    print(nums1,nums2)
-
-minimumcommn(nums1,nums2)
+    i,j=0,0
+    while i<len(nums1) and j<len(nums2):
+        if nums1[i]==nums2[j]:
+            return nums1[i]
+        elif nums1[i]<nums2[j]:
+            i+=1
+        else:
+            j+=1
+            
+    return -1
+nums1 = [2,4]
+nums2=[1,2]
+print(minimumcommn(nums1,nums2))
