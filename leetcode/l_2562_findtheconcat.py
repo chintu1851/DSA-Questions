e989b4d6405572f2c66a28b494266714ae26e32f
chintu1851 <patelchintan1851@gmail.com>
2025-04-06 20:29:07 -0400

    add code

--- leetcode/l_2562_findtheconcat.py
+++ leetcode/l_2562_findtheconcat.py
@@ -1,20 +1,17 @@
 def findconcat(nums):
-    print(nums)
-    i = 0
-    j = len(nums) - 1
-    value=0
-    while i < j:
-        print(int(str(nums[i]) + str(nums[j])))  # Fixed the issue here
-        value = value+int(str(nums[i]) + str(nums[j]))
-        i += 1
-        j -= 1
-    # print(value)
-    if len(nums) % 2 != 0:
-        print(nums[i])
-        value = value + int(str(nums[i]))
-        return value
-    elif len(nums) % 2 == 0:
-        return value
+        i = 0
+        j = len(nums) - 1
+        value=0
+        while i < j:
+            value = value+int(str(nums[i]) + str(nums[j]))
+            i += 1
+            j -= 1
+        if len(nums) % 2 != 0:
+            print(nums[i])
+            value = value + int(str(nums[i]))
+            return value
+        elif len(nums) % 2 == 0:
+            return value
    
 nums = [7,52,2,4]
 print(findconcat(nums))
