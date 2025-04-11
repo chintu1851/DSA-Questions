58d074de70a1f8e4e745e7ae9d9fe7118f12733b
chintu1851 <patelchintan1851@gmail.com>
2025-04-11 14:46:04 -0400

    add code

---
+++ leetcode/l_1652_defuse.py
@@ -0,0 +1,8 @@
+def decrypt(code,k):
+    print(code)
+    totalsum = sum(code)
+    print(totalsum)
+    
+code = [5,7,1,4]
+k = 3
+decrypt(code,k)
--- leetcode/l_75_sortcolor.py
+++ leetcode/l_75_sortcolor.py
@@ -1,17 +1,31 @@
 def sortcolor(nums):
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         zero = nums.count(0)
+#         one = nums.count(1)
+#         two = nums.count(2)
+#         for i in range(zero):
+#             nums[i] = 0
+#         for i in range(zero, zero + one):
+#             nums[i] = 1
+#         for i in range(zero + one, len(nums)):
+#             nums[i] = 2
+#         print(nums)
+# nums = [2,0,2,1,1,0]
+# sortcolor(nums)
+
+    low, mid, high = 0, 0, len(nums) - 1
+
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:  # nums[mid] == 2
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+
     print(nums)
-    i = 0
-    j= len(nums)-1
-    
-    while i < j:
-        if nums[i]==0:
-            nums[j] , nums[i] = nums[i] , nums[j]
-            i+=1
-            j-=1
-        elif nums[i]<nums[j]:
-            i+=1
-            j-=1
-            
-    print(nums)
-nums = [2,0,2,1,1,0]
-sortcolor(nums)
