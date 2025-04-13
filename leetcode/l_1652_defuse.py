7cbce13ec82816044cce4fa33bc6c58bfcc470e7
chintu1851 <patelchintan1851@gmail.com>
2025-04-12 23:40:55 -0400

    add code

--- leetcode/l_1652_defuse.py
+++ leetcode/l_1652_defuse.py
@@ -1,8 +1,15 @@
-def decrypt(code,k):
-    print(code)
-    totalsum = sum(code)
-    print(totalsum)
-    
+def decrypt(code, k):
+    n = len(code)
+    for i in range(n):
+        if k < 0:
+            result = [code[(i + k + j) % n] for j in range(abs(k))]
+            print(f"i = {i}, value = {code[i]} → previous {abs(k)}:", result)
+        elif k > 0:
+            result = [code[(i + 1 + j) % n] for j in range(k)]
+            print(f"i = {i}, value = {code[i]} → next {k}:", result)
+        else:
+            print(f"i = {i}, value = {code[i]} → No elements to show (k = 0)")
+
 code = [5,7,1,4]
-k = 3
+k = -2
 decrypt(code,k)
