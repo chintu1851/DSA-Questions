f73e7a23f2c572103123554a6199f3e1c0daad42
chintu1851 <patelchintan1851@gmail.com>
2025-04-14 13:49:01 -0400

    add code

--- leetcode/l_1763_nicesubstring.py
+++ leetcode/l_1763_nicesubstring.py
@@ -1,6 +1,12 @@
 def longset(s):
     if len(s)<2:
         return ""
-    
+    charset=set(s)
+    for i,st in enumerate(s):
+        if st.swapcase() not in charset:
+            leftside=longset(s[:i])
+            rightside = longset(s[i+1:])
+            return leftside if len(leftside)>len(rightside) else rightside
+    return s
 s='YazaAay'
-longset(s)
+print(longset(s))
