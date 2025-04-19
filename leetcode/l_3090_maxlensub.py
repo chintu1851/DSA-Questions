edceed0bc6f417e08712dffbceebeb0718a0771d
chintu1851 <patelchintan1851@gmail.com>
2025-04-19 12:38:05 -0400

    add code

--- leetcode/l_3090_maxlensub.py
+++ leetcode/l_3090_maxlensub.py
@@ -1,5 +1,18 @@
-def maxlength(s):
-    print(s)
+from collections import defaultdict
 
+def maxlength(s):
+    d = defaultdict(int)  # Creates a default dictionary with default value of 0
+    right=0
+    left = 0
+    Max = 0
+    for i in s:
+        right+=1
+        d[i]+=1
+        while d[i]>2:
+            print(d[s[left]])
+            d[s[left]]-=1
+            left+=1
+        Max=max(Max,right-left)
+    print(d)
 s = "bcbbbcba"
 maxlength(s)
