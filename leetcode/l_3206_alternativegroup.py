28b87b55efed73b3993eeef7b8fd9c61c17f5771
chintu1851 <patelchintan1851@gmail.com>
2025-04-21 16:44:52 -0400

    add code

--- leetcode/l_3206_alternativegroup.py
+++ leetcode/l_3206_alternativegroup.py
@@ -1,5 +1,15 @@
 def alternative(colors):
-    print(colors)
-    
-colors = [1,1,1]
+    count=0
+    n=len(colors)
+    for i in range(len(colors)):
+        a=colors[i]
+        b=colors[(i+1) % n]
+        c=colors[(i+2) % n]
+        print((i+1) % n,(i+2) % n)
+        if c==a and b!=a:
+            count+=1
+    print(count)
+  
+
+colors = [0, 1, 0, 0, 1]
 alternative(colors)
