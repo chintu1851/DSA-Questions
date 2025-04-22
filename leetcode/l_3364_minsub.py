fd85e807117cb34dc75666badbb5fdc21bb29623
chintu1851 <patelchintan1851@gmail.com>
2025-04-22 19:54:46 -0400

    add code

--- leetcode/l_3364_minsub.py
+++ leetcode/l_3364_minsub.py
@@ -5,16 +5,14 @@ def minPositiveSumSubarray(nums, l, r):
         prefix = [0] * (n + 1)
         for i in range(n):
             prefix[i + 1] = prefix[i] + nums[i]
-        print(prefix)
         minsum = float('inf')  # Initialize minsum to infinity
-
+        print(prefix)
         # Step 2: Use the prefix sum to efficiently calculate subarray sums
         for size in range(l, r + 1):
             print('this is size',size)
             for i in range(n - size + 1):
-                # print('size',i+size)
+                print("this is first step")
                 curr_sum   = prefix[i + size] - prefix[i]
-                print("prefixsumishere",prefix[i + size])
                 if curr_sum > 0:
                     minsum = min(minsum, curr_sum)
 
