eea303b397b440fd0fc6f93477c3ecb3d62c6980
chintu1851 <patelchintan1851@gmail.com>
2025-04-24 00:38:13 -0400

    add code

--- leetcode/l_209_minsizesum.py
+++ leetcode/l_209_minsizesum.py
@@ -1,10 +1,8 @@
 def minisizenum(target,nums):
-    prefix=[0]*len(nums)
-    prefix[0]=nums[0]
-    for i in range(1,len(nums)):
-        prefix[i] = prefix[i-1]+nums[i]
-        
-    print(prefix)
+    j=0
+    k=0
+    
+    
 
 target=7
 nums=[2,3,1,2,4,3]
