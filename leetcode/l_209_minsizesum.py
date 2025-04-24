4d264fe69ab3071aa945d0fe7e39480b47c18a61
chintu1851 <patelchintan1851@gmail.com>
2025-04-24 12:41:27 -0400

    add code

--- leetcode/l_209_minsizesum.py
+++ leetcode/l_209_minsizesum.py
@@ -1,8 +1,13 @@
 def minisizenum(target,nums):
-    j=0
-    k=0
-    
-    
+    l, s, m = 0, 0, float('inf')
+    for r in range(len(nums)):
+        s += nums[r]
+        while s >= target:
+            print("this is sum",s)
+            print("inside loop",l)
+            m = min(m, r - l + 1)
+            s -= nums[l]
+            l += 1
 
 target=7
 nums=[2,3,1,2,4,3]
