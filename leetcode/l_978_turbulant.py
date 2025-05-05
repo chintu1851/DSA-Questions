f56e1f4b0d3bc7b31b10887afcf1f7f7ccac2154
chintu1851 <patelchintan1851@gmail.com>
2025-05-05 16:27:28 -0400

    add code

--- leetcode/l_978_turbulant.py
+++ leetcode/l_978_turbulant.py
@@ -1,8 +1,22 @@
 def turbulent(arr):
-    left=0
-    right=0
-    res=0
-    prev=""
-    
-arr = [9,4,2,10,7,8,8,1,9]
-turbulent(arr)
+        l, r = 0, 1
+        res, prev = 1, ""
+
+        while r < len(arr):
+            if arr[r - 1] > arr[r] and prev != ">":
+                res = max(res, r - l + 1)
+                r += 1
+                prev = ">"
+            elif arr[r - 1] < arr[r] and prev != "<":
+                res = max(res, r - l + 1)
+                r += 1
+                prev = "<"
+            else:
+                r = r + 1 if arr[r] == arr[r - 1] else r
+                l = r - 1
+                prev = ""
+
+        return res
+
+arr = [9, 4, 2, 10, 7, 8, 8, 1, 9]
+print(turbulent(arr)) 
