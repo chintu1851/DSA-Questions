96e95d3bc1869c98898f12ab2f43f9f19ed283ed
chintu1851 <patelchintan1851@gmail.com>
2025-05-09 13:40:29 -0400

    add code

--- leetcode/l_58_lengthoflastword.py
+++ leetcode/l_58_lengthoflastword.py
@@ -1,6 +1,13 @@
 def length(s):
-    print(s)
-    new=s.split()
-    print(len(new[-1]))        
+    i=0
+    j=len(s)-1
+    
+    while j>=0 and s[j]==" ":
+        j-=1
+    print(j)
+    while j>=0 and s[j]!=" ":
+        i+=1
+        j-=1
+    print(i)
 s = "luffy is still joyboy"
 length(s)
