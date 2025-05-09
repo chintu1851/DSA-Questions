1920d13fcf64f13db6d94d2fe7c0ab935a6d43c2
chintu1851 <patelchintan1851@gmail.com>
2025-05-09 11:41:01 -0400

    add code

---
+++ leetcode/l_58_lengthoflastword.py
@@ -0,0 +1,6 @@
+def length(s):
+    print(s)
+    new=s.split()
+    print(len(new[-1]))        
+s = "luffy is still joyboy"
+length(s)
--- leetcode/l_997_findtowmjudge.py
+++ leetcode/l_997_findtowmjudge.py
@@ -4,17 +4,16 @@ def town_judge(n, trust):
     for pair in trust:
         a = pair[0]
         b = pair[1]
-        print(a,b)
+        
         trust_score[a] -= 1
         trust_score[b] += 1
-
-    print(trust_score)
+    print(trust_score)    
     for i in range(1, n + 1):
         if trust_score[i] == n - 1:
             print(i)
             return
     print(-1)
 
-n=2
-trust=[[1,2]]
+n = 3
+trust = [[1,3],[2,3]]
 town_judge(n,trust)
