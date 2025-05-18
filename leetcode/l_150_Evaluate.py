7f3b6b43a16b827ce453f0470a021846d684cd0d
chintu1851 <patelchintan1851@gmail.com>
2025-05-18 14:57:30 -0400

    add code

--- leetcode/l_150_Evaluate.py
+++ leetcode/l_150_Evaluate.py
@@ -1,21 +1,28 @@
 def evaluate(tokens):
-    
-    stack=[]
-    for num in range(len(tokens)):
-        if tokens[num]=="+":
-            stack.append(stack.pop()+stack.pop())
-        elif tokens[num]=="-":
-            a=stack.pop()
-            b=stack.pop()
-            stack.append(b-a)
-        elif tokens[num]=="*":
-            stack.append(stack.pop()*stack.pop())
-        elif tokens[num]=="/":
-            a=stack.pop()
-            b=stack.pop()
-            stack.append(b/a)
-        else:
-            stack.append(tokens[num])
-    print(stack)
-tokens = ["2","1","+","3","*"]
-evaluate(tokens)
+    stack = []
+
+    for tok in tokens:
+        if tok == "+":
+            stack.append(stack.pop() + stack.pop())
+
+        elif tok == "-":
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b - a)
+
+        elif tok == "*":
+            stack.append(stack.pop() * stack.pop())
+
+        elif tok == "/":
+            a = stack.pop()
+            b = stack.pop()
+            # truncate toward 0 (LeetCode spec); use int() on the float result
+            stack.append(int(b / a))      
+
+        else:  # operand
+            stack.append(int(tok))        # <‑‑ convert to int here
+
+    return stack[0]                      # final result
+
+tokens = ["2", "1", "+", "3", "*"]
+print(evaluate(tokens))   # → 9
