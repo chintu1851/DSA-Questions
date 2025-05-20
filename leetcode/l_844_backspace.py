76e454e31c8169c76e9c9d66fdf2167ade083808
chintu1851 <patelchintan1851@gmail.com>
2025-05-20 09:52:05 -0400

    add code

--- leetcode/l_844_backspace.py
+++ leetcode/l_844_backspace.py
@@ -1,19 +1,14 @@
 def checkbackspace(s,t):
-        stack1=[]
-        stack2=[]
-        
-        for val in range(len(s)):
-            stack1.append(s[val])
-            if stack1 and s[val]=="#":
-                stack1.pop()
-        print(stack1)       
-        for val in range(len(t)):
-            stack2.append(t[val])
-            if stack2 and t[val]=="#":
-                stack2.pop()
-                
-        print(stack2)    
-        return True if stack1==stack2 else False
+        def remove_characters(s):
+            stack = []
+            for char in s:
+                if char == '#' and stack:
+                    stack.pop()
+                elif char != '#':
+                    stack.append(char)
+            return stack
+
+        return remove_characters(s) == remove_characters(t)
     
 s ="xywrrmp"
 t ="xywrrmu#p"
