0b29a4a6b19ff99911b663de51fa40212ed04e48
chintu1851 <patelchintan1851@gmail.com>
2025-05-21 10:54:03 -0400

    add code

--- leetcode/l_1544_stringgeat.py
+++ leetcode/l_1544_stringgeat.py
@@ -1,10 +1,15 @@
 def makestringgreat(s):
     
     print(s)
-    
-    for i in s:
-        print(ord(i)-ord('a'))
-    
+    strng=list(s)
+    stack=[]
+    for i in range(len(strng)):
+        if stack and abs((ord(stack[-1])-ord(strng[i]))==32):
+            stack.pop()
+        else:
+            stack.append(strng[i])
+        
+    print("".join(stack))    
 s = "leEeetcode"
 makestringgreat(s)
 
