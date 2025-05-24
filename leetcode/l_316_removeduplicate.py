6915e744bc0e74436fc52521a91fe7772d5ef251
chintu1851 <patelchintan1851@gmail.com>
2025-05-24 13:24:04 -0400

    add code

--- leetcode/l_316_removeduplicate.py
+++ leetcode/l_316_removeduplicate.py
@@ -1,5 +1,18 @@
 def removeduplicate(s):
     last_index = {char: i for i, char in enumerate(s)}  # Last occurrence
-    print(last_index)
+    
+    stack=[]
+    seen=set()
+    for i,char in enumerate(s):
+        if char not in seen:
+            
+            while stack and char<stack[-1] and i<last_index[stack[-1]]:
+                seen.remove(stack.pop())
+            
+            seen.add(char)
+            stack.append(char)
+                
+        
+    return "".join(stack)
 s="cbacdcbc"
 print(removeduplicate(s))
