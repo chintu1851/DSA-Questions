25d22468ec68dfe64fa76f012cdfe53df7ef5cb1
chintu1851 <patelchintan1851@gmail.com>
2025-05-25 18:41:35 -0400

    add code

--- leetcode/l_402_removedigit.py
+++ leetcode/l_402_removedigit.py
@@ -1,6 +1,22 @@
-def removedigit(num,k):
-    print(num)
+def removedigit(num, k):
+    stack = []
+    for i in num:
+        while stack and stack[-1] > i and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(i)
 
-num = "1432219"
+    # Corrected the condition here
+    while k > 0:
+        stack.pop()
+        k -= 1
+
+    result = ''.join(stack).lstrip('0')
+    print("Final Stack:", stack)
+    print("Result after removing leading 0s:", result)
+    return result if result else "0"
+
+    
+num = "123456"
 k = 3
 removedigit(num,k)
