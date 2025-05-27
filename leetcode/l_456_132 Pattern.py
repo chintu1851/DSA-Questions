6bcc923ab2211ac3c6a9d7a9543d05c7757e4028
chintu1851 <patelchintan1851@gmail.com>
2025-05-27 12:19:15 -0400

    Add code

--- leetcode/l_456_132 Pattern.py
+++ leetcode/l_456_132 Pattern.py
@@ -1,5 +1,15 @@
 def pattern(nums):
+    stack = []
+    second = float('-inf')
     
-    print(nums)
-nums = [1,2,3,4]
-pattern(nums)
+    for i in range(len(nums) - 1, -1, -1):  # Fixed the loop
+        if nums[i] < second:
+            return True
+        while stack and stack[-1] < nums[i]:
+            second = stack.pop()
+        print(stack)
+        stack.append(nums[i])
+        print(stack)
+    return False  # if no pattern found
+nums = [3, 1, 4, 2]
+print(pattern(nums))  # Output: True
