6363d6523be7e8063ed5fc30a5fdc529952082dc
chintu1851 <patelchintan1851@gmail.com>
2025-05-29 13:40:39 -0400

    Add code

---
+++ leetcode/l_503_Next Greater Element II.py
@@ -0,0 +1,12 @@
+def maxelements(nums):
+    stack=[]
+    n=len(nums)
+    result=[-1] * len(nums)
+    for i in range(2*n):
+        while stack and nums[stack[-1]]<nums[i%n]:
+            result[stack.pop()] = nums[i%n]
+        if i<n:
+            stack.append(i)
+    return result
+nums = [1,2,1]
+print(maxelements(nums))
