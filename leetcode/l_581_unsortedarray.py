a79c6e8106e81dbb96fbe50fb439eceab39a71f5
chintu1851 <patelchintan1851@gmail.com>
2025-05-30 10:42:34 -0400

    Add code

---
+++ leetcode/l_581_unsortedarray.py
@@ -0,0 +1,19 @@
+def unsortedarray(nums):
+    left=len(nums)-1
+    right=0
+    stack=[]
+    for i in range(len(nums)):
+        while stack and nums[stack[-1]]>nums[i]:
+            print(stack[-1])
+            left=min(left,stack.pop())
+        stack.append(i)
+    print(stack)
+    stack=[]        
+    for i in range(len(nums)-1,-1, -1):
+        while stack and nums[stack[-1]]<nums[i]:
+            right = max(right, stack.pop())
+        stack.append(i)
+        
+    print(stack)
+nums = [2, 6, 4, 8, 10, 9, 15]
+unsortedarray(nums)
