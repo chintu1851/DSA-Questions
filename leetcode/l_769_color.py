8cab80464048abe58ee7f20fce717c92749265d1
chintu1851 <patelchintan1851@gmail.com>
2025-06-02 01:13:30 -0400

    add code

---
+++ leetcode/l_769_color.py
@@ -0,0 +1,13 @@
+def maxchunk(arr):
+        ans = 0
+        prev_max = 0
+        for idx, x in enumerate(arr):
+            prev_max = max(prev_max, x)
+            print(prev_max)
+            if prev_max == idx:
+                print('this is idx',idx)
+                ans += 1
+        return ans
+
+arr = [1,0,2,3,4]
+print(maxchunk(arr))
