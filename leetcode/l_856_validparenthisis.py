1db92c25266e81b408b876e974a4ed9605dfe319
chintu1851 <patelchintan1851@gmail.com>
2025-06-04 11:48:59 -0400

    add code

---
+++ leetcode/l_856_validparenthisis.py
@@ -0,0 +1,17 @@
+def scoreofparenthisis(s):
+    stack = []
+    score = 0
+
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(score)
+            score = 0
+        else:
+            last = stack.pop()
+            score = last + max(2 * score, 1)
+        
+    return score
+
+
+s = "(()(()))"
+print(scoreofparenthisis(s))
