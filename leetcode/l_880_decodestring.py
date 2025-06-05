a4fa50267bb3d0a0d37a4ecd9eaa540eb27b140e
chintu1851 <patelchintan1851@gmail.com>
2025-06-05 19:57:32 -0400

    Add code

--- leetcode/l_880_decodestring.py
+++ leetcode/l_880_decodestring.py
@@ -1,7 +1,24 @@
-def decodestring(s,k):
-    
-    print(k,s)
+def decodestring(s, k):
+    size=0
+    # First, calculate the total size of the decoded string
+    for char in s:
+        if char.isdigit():
+            size *= int(char)
+        else:
+            size += 1
+    for i in range(len(s) - 1, -1, -1):
+        char = s[i]
+        k %= size  # Reduce k within current size
+        print(k,size)
+        if k == 0 and char.isalpha():
+            return char
 
-
-s = "leet2code3", k = 10
-decodestring(s,k)
+        if char.isdigit():
+            size //= int(char)
+        else:
+            size -= 1
+    print(size)
+# Test
+s = "leet2code3"
+k = 10
+decodestring(s, k)
