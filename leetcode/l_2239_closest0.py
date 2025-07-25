5d644d3ec7023ba5bdb93ea07b9007153839a416
chintu1851 <patelchintan1851@gmail.com>
2025-07-25 00:03:38 -0400

    add code

---
+++ leetcode/l_2239_closest0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest = nums[0]
+        for num in nums:
+            if abs(num) < abs(closest):
+                closest = num
+            elif abs(num) == abs(closest):
+                closest = max(closest, num)
+        return closest
